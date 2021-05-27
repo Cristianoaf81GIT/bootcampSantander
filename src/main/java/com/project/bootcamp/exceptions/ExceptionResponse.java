d57e067e2c73c191f1fbe63e8ee418a07b04a657
cristiano alexandre de faria <cristianoaf81@gmail.com>
2021-05-27 16:47:03 -0300

    final

---
+++ src/main/java/com/project/bootcamp/exceptions/ExceptionResponse.java
@@ -0,0 +1,11 @@
+package com.project.bootcamp.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+public class ExceptionResponse {
+    private String message;
+}
---
+++ src/main/java/com/project/bootcamp/util/MessageUtils.java
@@ -0,0 +1,6 @@
+package com.project.bootcamp.util;
+
+public abstract class MessageUtils {    
+    public static final String Stock_ALREADY_EXISTS = "Stock already exists in database";
+    public static final String NO_RECORDS_FOUND = "No records found";
+}
